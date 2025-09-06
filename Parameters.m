03336581650f074b0867c123da283429dd52d054
sm103 <sm103@cummins.com>
2025-09-06 23:57:03 +0530

    Initial commit

---
+++ Parameters.m
@@ -0,0 +1,44 @@
+Fc_EMF = 60;
+Fc_W = 60;
+Fsw = 20000;
+
+J = 0.01;
+Ld = 0.00047;
+Lq = 0.00047;
+P = 4;
+Psi = 0.1;
+Te_max = 80;
+Wm_max = 3000;
+theta_offset = 0;
+Pmax = 50000;
+Rs = 0.079;
+
+Vdc = 370;
+Imax = 95;
+
+m= 0.06;
+
+Ts = 2e-6;
+Tsc = 5e-05;
+
+
+theta_e_diff1 = 0.35;
+theta_e_diff2 = 0.15;
+
+K_SMO = 20;
+Ki_Id = 1464;
+Ki_Iq = 1464;
+Kp_Id = 7.3;
+Kp_Iq = 7.3;
+Ki_w = 210;
+Kp_w = 2.9;
+
+
+a = 0.01;
+
+K_alpha = 1.5;
+K_beta = 1.5;
+
+Kp_PLL = 1;
+Ki_PLL = 9;
+Kb_PLL = 50;
---
+++ README.md
@@ -0,0 +1,9 @@
+# 7160CEM---MTech-Automotive-Software-Engineering-Project-
+This repository contains Simulink model for Fault Tolerant Motor Control for EV and HEV
+
+Steps to run the model
+1) Run Parameters.m file
+2) Then simulate the model
+3) Fault will be inserted at t = 0.7 seconds
+4) Output Measurment block inside the Controller subsystem will constain measurment scope for various parameter like speed, position etc.
+5) Additional parameters can be monitored from Data Inspector tab
